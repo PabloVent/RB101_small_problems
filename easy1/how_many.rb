fffabb44dcec80fb1d2266521259c77cf1d923fc
Pablo Vent <ventpdev@gmail.com>
2019-08-19 23:45:21 +0100

    further exploration

--- easy1/how_many.rb
+++ easy1/how_many.rb
@@ -2,7 +2,7 @@ vehicles = [
   'car', 'car', 'truck', 'car', 'SUV', 'truck',
   'motorcycle', 'motorcycle', 'car', 'truck'
 ]
-
+vehicles = vehicles.map(&:downcase)
 def counter arr
   arr.uniq.each do |el|
     puts "#{el} => #{arr.count(el)}"
