c3a7ce135325446792c97e5c516e9a3b57ab9d47
Pablo Vent <ventpdev@gmail.com>
2019-10-10 08:02:48 +0100

    exercise 1 further exploration refactored

--- easy2/how_old_teddy.rb
+++ easy2/how_old_teddy.rb
@@ -1,10 +1,10 @@
-puts "Please provide a name:"
-name = gets.chomp
-
 def persons_age(name)
   age = rand(20..200)
   name.empty? ? "Teddy is #{age} years old." : "#{name} is #{age} years old."
 end
+
+puts "Please provide a name:"
+name = gets.chomp
 
 puts persons_age(name)
 
