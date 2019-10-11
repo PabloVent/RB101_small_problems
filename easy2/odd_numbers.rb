e11a3205347af832ffe2d097022e78a6999283bf
Pablo Vent <ventpdev@gmail.com>
2019-10-11 08:10:33 +0100

    exercise 6 refactored

--- easy2/odd_numbers.rb
+++ easy2/odd_numbers.rb
@@ -1,7 +1,6 @@
 arr = Array.new(100){|j| j}
 
 odd_arr = arr.select { |odd_el| odd_el.odd? }
-
 puts odd_arr
 
 # Further Exploration
@@ -11,3 +10,6 @@ while idx < arr.size
   puts arr[idx] if idx.odd?
   idx += 1
 end
+
+# or
+1.upto(99) {|j| puts j if j.odd?}
