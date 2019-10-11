6b986a12b5cc179a6bfba1f5e34f05eeb8945f89
Pablo Vent <ventpdev@gmail.com>
2019-10-11 07:57:50 +0100

    exercise 6 completed

---
+++ easy2/odd_numbers.rb
@@ -0,0 +1,13 @@
+arr = Array.new(100){|j| j}
+
+odd_arr = arr.select { |odd_el| odd_el.odd? }
+
+puts odd_arr
+
+# Further Exploration
+
+idx = 0
+while idx < arr.size
+  puts arr[idx] if idx.odd?
+  idx += 1
+end
