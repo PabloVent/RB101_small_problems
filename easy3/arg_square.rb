c4e44967e93158ee3c71c696ff7f92dcb8785578
Pablo Vent <ventpdev@gmail.com>
2019-10-15 07:58:13 +0100

    Exercise 6 completed.

--- easy3/arg_square.rb
+++ easy3/arg_square.rb
@@ -7,3 +7,23 @@ def square(num)
 end
 
 # p square(-8) == 64
+
+# Further exploration
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+def to_the_power(num, power=1)
+  if power == 2
+    multiply(num, num)
+  elsif power == 0
+    1
+  elsif power == 1
+    num
+  elsif power > 2
+    num ** power
+  end
+end
+
+p to_the_power(2, 3)
